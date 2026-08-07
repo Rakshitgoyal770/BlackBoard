3445e664f478eb6168266ad94e5350db05746326
Rakshitgoyal770 <rakshitgoyal770@gmail.com>
2026-08-08 01:06:42 +0530

    updated ui and icons

---
+++ my-turborepo/apps/frontend/src/componants/cameraIcon.tsx
@@ -0,0 +1,17 @@
+type CameraIconProps = React.SVGProps<SVGSVGElement>;
+
+export default function CameraIcon(props: CameraIconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      width="24"
+      height="24"
+      aria-hidden="true"
+      {...props}
+    >
+      <path d="M4.5 4.5a3 3 0 0 0-3 3v9a3 3 0 0 0 3 3h8.25a3 3 0 0 0 3-3v-9a3 3 0 0 0-3-3H4.5ZM19.94 18.75l-2.69-2.69V7.94l2.69-2.69c.944-.945 2.56-.276 2.56 1.06v11.38c0 1.336-1.616 2.005-2.56 1.06Z" />
+    </svg>
+  );
+}
